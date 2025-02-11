43f8493d718dd6ff6012613863f46b96f46e8a6f
Inchino <alessandro.incalza22@gmail.com>
2025-02-11 15:40:32 +0100

    Fine esercizio

---
+++ S1-L2/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Esercizio2
+{
+    public class Persona
+    {
+        private string _nome = string.Empty;
+        private string _cognome = string.Empty;
+        private int _eta = 0;
+
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value ?? string.Empty;
+        }
+
+        public string Cognome
+        {
+            get => _cognome;
+            set => _cognome = value ?? string.Empty;
+        }
+
+        public int Eta
+        {
+            get => _eta;
+            set => _eta = value >= 0 ? value : 0;
+        }
+
+        public void Saluti()
+        {
+            Console.WriteLine($"Salve, mi chiamo {Nome} {Cognome} ed ho {Eta} anni!");
+        }
+    }
+}
---
+++ S1-L2/Program.cs
@@ -0,0 +1,10 @@
+﻿using Esercizio2;
+
+var persona = new Persona()
+{
+    Nome = "Alessandro",
+    Cognome = "Incalza",
+    Eta = 22,
+};
+
+persona.Saluti();
